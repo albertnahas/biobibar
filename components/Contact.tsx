91cadd981d48e60e648daed84fd692f366d7ef13
Tetiana Pomohaieva <t.pomogaeva@gmail.com>
2023-01-13 22:21:29 +0100

    add Input component

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -1,3 +1,5 @@
+import Input from "./Input";
+
 export const Contact = () => {
   return (
     <section className="contact">
@@ -5,13 +7,13 @@ export const Contact = () => {
       <div className="container">
         <form>
           <div className="grid gap-8 px-12 md:grid-cols-4 md:px-24">
-            <input type="text" className="input" placeholder="Name" />
-            <input type="text" className="input" placeholder="Phone" />
-            <input type="email" className="input" placeholder="Email Address" />
+            <Input type="text" className="input" placeholder="Name" />
+            <Input type="text" className="input" placeholder="Phone" />
+            <Input type="email" className="input" placeholder="Email Address" />
             <button className="btn-secondary uppercase">Contact us</button>
           </div>
         </form>
       </div>
     </section>
-  )
-}
+  );
+};

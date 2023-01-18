1b6633ffecdb0876d40b058e1a6ce4b05334739f
AlbertT <albertonahas992@gmail.com>
2023-01-19 00:22:32 +0100

    add pages loading

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,5 @@
 import "../styles/globals.css"
+import "../styles/loader.css"
 import type { AppProps } from "next/app"
 import { ProductProvider } from "../contexts/ProductsContext"
 import AOS from "aos"
@@ -8,18 +9,32 @@ import { useEffect } from "react"
 import "slick-carousel/slick/slick.css"
 import "slick-carousel/slick/slick-theme.css"
 import "react-toastify/dist/ReactToastify.css"
-import { toast, ToastContainer } from "react-toastify"
+import { ToastContainer } from "react-toastify"
+import NProgress from "nprogress"
+import Router from "next/router"
 
 export default function App({ Component, pageProps }: AppProps) {
   useEffect(() => {
     AOS.init()
   }, [])
 
+  Router.events.on("routeChangeStart", (url) => {
+    NProgress.start()
+  })
+  Router.events.on("routeChangeComplete", (url) => {
+    NProgress.done()
+  })
+  Router.events.on("routeChangeError", (url) => {
+    NProgress.done()
+  })
+
   return (
-    <ProductProvider>
-      <SEO />
-      <ToastContainer />
-      <Component {...pageProps} />
-    </ProductProvider>
+    <>
+      <ProductProvider>
+        <SEO />
+        <ToastContainer />
+        <Component {...pageProps} />
+      </ProductProvider>
+    </>
   )
 }

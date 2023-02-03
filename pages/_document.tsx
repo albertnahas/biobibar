a04e870da7097d87b56cbe238aeeec4da1dde134
AlbertT <albertonahas992@gmail.com>
2023-02-03 16:53:42 +0100

    fix chat

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,13 +1,15 @@
-import { Html, Head, Main, NextScript } from "next/document"
+import { Html, Head, Main, NextScript } from 'next/document'
+import Facebook from '../components/Facebook'
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head>
-        <link rel="icon" href="/favicon.png" />
-      </Head>
+       <Head>
+          <link rel="icon" href="/favicon.png" />
+        </Head>
       <body>
         <Main />
+        <Facebook />
         <NextScript />
       </body>
     </Html>
